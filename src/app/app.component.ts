ccf505d32da2f822629c942853ca5d8bb7fc5a41
winuss <niceyss@gmail.com>
2020-05-05 18:31:11 +0900

    f

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'project-pwa';
+export class AppComponent implements OnInit {
+
+    updateMessage = 'project-pwa';
+
+    constructor(private swUpdate: SwUpdate) {
+
+    }
+
+    ngOnInit(): void {
+        if ('serviceWorker' in navigator) {
+        console.log('service worker!!!!!!!!!!!!!');
+        
+        if (this.swUpdate.isEnabled) {
+            this.swUpdate.available.subscribe(() => {
+            if (confirm('New version available. Load New Version?')) {
+                window.location.reload(); // 재시작하면 캐시된 데이터를 버리고 새 버전의 코드를 불러와 적용한다.
+            }
+            });
+        }
+        }
+        else {
+            console.log('service worker is not ready');
+        }
+    }
+
 }
